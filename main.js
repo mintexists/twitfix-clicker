355821b4546a61aa58ce16aa100a10c92442be7a
nevin <60362123+enbyautumn@users.noreply.github.com>
2022-02-27 20:00:54 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let counter = document.getElementById("count")
-let url = "https://cors.evaexists.workers.dev/?url=https://fxtwitter.com/api/stats/"
+let url = "https://fxtwitter.com/api/stats/"
 
 counter.addEventListener("click", e => {
     fetch(url).then(r => r.json()).then(j => counter.innerText = j.api)
